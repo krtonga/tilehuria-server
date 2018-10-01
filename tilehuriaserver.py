4ccfb791cf848d28555b61284632c6d15dc8c803
Chelsea Voss <voss.chelsea@gmail.com>
2018-09-30 20:36:09 -0700

    Add placeholder endpoint for form submission
    Right now, it just returns the file that the user uploaded.

--- tilehuriaserver.py
+++ tilehuriaserver.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import tempfile
+
+from flask import Flask, request, send_file
 
 application = Flask(__name__)
 
@@ -11,6 +13,16 @@ def hello():
 @application.route("/")
 def root():
     return open("pages/index.html", "r").read()
+
+
+@application.route("/polygon2mbtiles", methods=["POST"])
+def polygon2mbtiles():
+    polygon_filestorage = request.files["polygon"]  # type: FileStorage
+
+    with tempfile.NamedTemporaryFile("w+b") as polygon_tempfile:
+        polygon_filestorage.save(polygon_tempfile)
+        polygon_tempfile.seek(0)
+        return send_file(polygon_tempfile.name, mimetype="image/jpg")
 
 
 if __name__ == "__main__":
