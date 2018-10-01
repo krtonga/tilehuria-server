817c218d9ee0f0450c94b4a28233de8df2968349
Chelsea Voss <voss.chelsea@gmail.com>
2018-09-30 19:53:13 -0700

    Serve index.html as root

--- tilehuriaserver.py
+++ tilehuriaserver.py
@@ -1,9 +1,17 @@
 from flask import Flask
+
 application = Flask(__name__)
 
-@application.route("/")
+
+@application.route("/hello")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+
+@application.route("/")
+def root():
+    return open("pages/index.html", "r").read()
+
+
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host="0.0.0.0")
